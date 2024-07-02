0243470659744e399f0e663a4eeed9f44d8015ad
bkh4149 <bkh4149@gmail.com>
2024-07-02 20:38:50 +0900

    ジャンルをテーブルで表示（test3）

---
+++ test3/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    syukei_dic = {
+        '雑学': 10, '動物': 6, '天体': 2, '地理': 2,
+        '食物': 1, '言語': 1, 'スペイン語': 1,
+        '美術': 1, '通貨': 1, '経済': 1
+    }
+    return render_template('index.html', syukei_dic=syukei_dic)
+
+@app.route('/select', methods=['POST'])
+def select():
+    selected_category = request.json.get('category')
+    return jsonify({'category': selected_category})
+
+if __name__ == '__main__':
+    app.run(debug=True)
